0a2cc0f2928299aee6cefcef5cbda3d642ec4f81
csalazara <csalazara@ucenfotec.ac.cr>
2024-07-14 12:34:20 -0600

    Creación de la pantalla de cambio de contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { AdminRoleGuard } from './guards/admin-role.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GuestGuard } from './guards/guest.guard';
 import { IRole } from './interfaces';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,11 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SigUpComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'resetPassword',
+    component: ResetPasswordComponent,
     canActivate: [GuestGuard],
   },
   {
