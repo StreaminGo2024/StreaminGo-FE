327a2406a77ff16fad121a01572272a29df73deb
luisdiegobando <lonandos@ucenfotec.ac.cr>
2024-07-21 13:47:21 -0600

    Gestion de perfil en el Front-end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { AdminRoleGuard } from './guards/admin-role.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GuestGuard } from './guards/guest.guard';
 import { IRole } from './interfaces';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   {
@@ -62,6 +63,16 @@ export const routes: Routes = [
             IRole.user
           ],
           name: 'Dashboard'
+        }
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        data: { 
+          authorities: [
+            IRole.user
+          ],
+          name: 'profile'
         }
       }
     ],
