0a6c9ba81049f9476baeb0b783b4201557b134f7
csalazara <csalazara@ucenfotec.ac.cr>
2024-07-25 16:56:30 -0600

    Mostrar el nombre del usuario en el nav

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -8,13 +8,15 @@ import { AuthService } from "../../services/auth.service";
   templateUrl: "./my-account.component.html",
 })
 export class MyAccountComponent implements OnInit {
-  public userName: string = 'Test';
+  public userName: string = '';
   private service = inject(AuthService);
 
   constructor(public router: Router) {
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userName = this.service.getUser()?.name || '';
+  }
 
   logout() {
     this.service.logout();
