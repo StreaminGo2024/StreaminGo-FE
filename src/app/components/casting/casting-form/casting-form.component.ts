cb19bd7034c7f0732aaa6dc63a79d08555982781
csalazara <csalazara@ucenfotec.ac.cr>
2024-07-28 11:46:36 -0600

    Validaciones insert casting

--- src/app/components/casting/casting-form/casting-form.component.ts
+++ src/app/components/casting/casting-form/casting-form.component.ts
@@ -1,38 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IActor, ICasting, ICastingActor } from '../../../interfaces';
-import { MatSelectModule } from '@angular/material/select'
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-casting-form',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatSelectModule
   ],
   templateUrl: './casting-form.component.html',
-  styleUrl: './casting-form.component.scss'
+  styleUrls: ['./casting-form.component.scss']
 })
-export class CastingFormComponent{
-
+export class CastingFormComponent implements OnInit {
   @Input() casting: ICasting = {};
-  @Input() actorList: IActor[] = []; 
-  public selectedActors: [] = [];
-
+  @Input() actorList: IActor[] = [];
   @Input() action = '';
   @Output() callParentEvent: EventEmitter<ICastingActor> = new EventEmitter<ICastingActor>();
 
+  public castingForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.castingForm = this.fb.group({
+      name: [this.casting.name || '', Validators.required],
+      selectedActors: [[], Validators.required]
+    });
+  }
 
   callEvent() {
+    if (this.castingForm.invalid) {
+      this.castingForm.markAllAsTouched();
+      return;
+    }
 
     const eventPayload: ICastingActor = {
-      casting: this.casting,
-      selectedActors: this.selectedActors
+      casting: {
+        ...this.casting,
+        name: this.castingForm.get('name')?.value
+      },
+      selectedActors: this.castingForm.get('selectedActors')?.value
     };
-    
+
     this.callParentEvent.emit(eventPayload);
   }
 }
-
