d0fc8ad673dee65f24c658524c910996193f58a2
José D Segura <j.davidsegura20@gmail.com>
2024-08-16 18:30:54 -0600

    Commit Bug - Forma de agregar Casting
    Bug - Forma de agregar Casting

--- src/app/components/movie/movie-form/movie-form.component.ts
+++ src/app/components/movie/movie-form/movie-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ICasting, IGenre, IMovie } from '../../../interfaces';
 
@@ -13,13 +13,21 @@ import { ICasting, IGenre, IMovie } from '../../../interfaces';
   templateUrl: './movie-form.component.html',
   styleUrls: ['./movie-form.component.scss']
 })
-export class MovieFormComponent {
+export class MovieFormComponent implements OnChanges{
   @Input() movie: IMovie = {};
   @Input() genreList: IGenre[] = [];
   @Input() castingList: ICasting[] = [];
   @Input() action = '';
   @Output() callParentEvent: EventEmitter<IMovie> = new EventEmitter<IMovie>();
   @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
+
+  filteredGenreList: IGenre[] = [];
+  showOptionsListGenre = false;
+  selectedGenreName: string = '';
+
+  filteredCastingList: ICasting[] = [];
+  showOptionsListCasting = false;
+  selectedCastingName: string = '';
 
   callEvent(form: NgForm) {
     if (form.valid && this.movie.imageCover && this.movie.video) {
@@ -89,6 +97,8 @@ export class MovieFormComponent {
     this.movie = {}; 
     this.movie.imageCover = '';
     this.movie.video = '';
+    this.selectedGenreName = '';
+    this.selectedCastingName = '';
   
     // Limpia las entradas de archivos
     const imageUploadInput = document.getElementById('imageUpload') as HTMLInputElement;
@@ -100,6 +110,72 @@ export class MovieFormComponent {
   
     if (videoUploadInput) {
       videoUploadInput.value = ''; 
+    }
+  }
+
+  filterGenres(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    this.filteredGenreList = this.genreList.filter(genre => 
+      genre.name?.toLowerCase().includes(input.toLowerCase())
+    );
+  }
+
+  filterCastings(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    this.filteredCastingList = this.castingList.filter(casting => 
+      casting.name?.toLowerCase().includes(input.toLowerCase())
+    );
+  }
+
+  selectGenre(genre: IGenre) {
+    if (genre.name) {
+      this.selectedGenreName = genre.name;
+    }
+    this.movie.genre = genre;
+    this.showOptionsListGenre = false;
+  }
+
+  selectCasting(casting: ICasting) {
+    if (casting.name) {
+      this.selectedCastingName = casting.name;
+    }
+    this.movie.casting = casting;
+    this.showOptionsListCasting = false;
+  }
+
+
+  showOptionsGenre() {
+    this.showOptionsListGenre = true;
+  }
+
+  showOptionsCasting(){
+    this.showOptionsListCasting = true;
+  }
+
+  hideOptionsGenre() {
+    setTimeout(() => this.showOptionsListGenre = false, 200);
+  }
+
+  hideOptionsCasting(){
+    setTimeout(() => this.showOptionsListCasting = false, 200);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['movie'] && this.movie.genre) {
+      const genre = this.movie.genre;
+      if (genre && genre.name) {
+        this.selectedGenreName = genre.name;
+      } else {
+        this.selectedGenreName = '';
+      }
+    }
+    if (changes['movie'] && this.movie.casting) {
+      const casting = this.movie.casting;
+      if (casting && casting.name) {
+        this.selectedCastingName = casting.name;
+      } else {
+        this.selectedCastingName = '';
+      }
     }
   }
   
